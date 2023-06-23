b4c49df6ed4784bc003f4396a3f1be1328a8f20d
Kamila Malikova <kamilamalikova173@gmail.com>
2023-06-23 08:58:59 +0700

    Spring-08 - Refactor

--- spring-08/src/main/java/ru/otus/library/db/dao/BooksDao.java
+++ spring-08/src/main/java/ru/otus/library/db/dao/BooksDao.java
@@ -1,6 +1,5 @@
 package ru.otus.library.db.dao;
 
-import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ru.otus.library.db.models.Book;
 
@@ -8,6 +7,4 @@ import ru.otus.library.db.models.Book;
  * CRUD operations with Books.
  */
 public interface BooksDao extends MongoRepository<Book, String> {
-  @Override
-  List<Book> findAll();
 }
